7d3daba4dd53fc243fc682566995d395b70617d4
MartinAlejandro1983 <martin.alejandro.islas@gmail.com>
2023-06-08 12:45:10 +0700

    Commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Attach an event listener to the submit button
+// This function will be called when the user clicks the submit button
+function calculate() {
+    // Get the values of the input fields
+    const happy1 = document.querySelector("input[id=happiness1]").value;
+    console.log(happy1);
+  }
+  
+  document.querySelector("button").addEventListener('click', calculate);
+  
+
+
+
+// Perform the calculation
+
+
+// Display the result of the calculation
+//document.querySelector("body").innerHTML += `<p>The sum is ${sum}</p>`;
+//}
+//if(prop("%") >= 84, "This is your Ikigai", if(prop("%") >= 70, "You might need to improve on one of these factors", if(prop("%") >= 60, "Maybe keep on brainstorming a bit...", if(prop("%") >= 15, "Aboslutely NOT your Ikigai", if(prop("%") >= 5, "Just no...", slice(" ", 0))))))
